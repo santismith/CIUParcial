b65de39d1ef94774175460725a5b4aa407793c81
santismith <santii.smiith@hotmail.com>
2021-09-27 18:47:27 -0300

    Terminado

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ var resultado = document.getElementById('resultado')
 var expRegularDNI = /^\d{8}(?:[-\s]\d{4})?$/
 
 
-botonIngreso.addEventListener("click", function(){
-    
+botonIngreso.addEventListener("click", function(evento){
+    evento.preventDefault()
     if(inputUsuario.value == null || inputUsuario.value.length == 0 || /^\s+$/.test(inputUsuario.value ) ){
         inputUsuario.classList.add("invalid")
         inputContrasenia.classList.add("invalid")
         resultado.innerHTML = '<p class="error"><strong>Error: </strong>¡Los campos no deben estar vacios!</p>'
+        return;
     }else if (expRegularDNI.test(inputUsuario.value) != true){
         inputUsuario.classList.add("invalid")
         resultado.innerHTML = '<p class="error"><strong>Error: </strong>¡Formato DNI Inválido!</p>'
+        return;
     }else if(inputContrasenia.value.length < 4){
         inputContrasenia.classList.add("invalid")
         resultado.innerHTML = '<p class="error"><strong>Error: </strong>¡La contraseña debe tener al menos 4 caracteres!</p>'
+        return;
     }
+    location.href= 'paginaprincipal.html'
     
-
-
 })
 
